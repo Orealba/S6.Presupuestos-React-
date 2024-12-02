9cfd22bfcdd9dfd3701f0a33d1039f68a1c3d6b6
Orealba Soriano <orealbas@gmail.com>
2024-12-02 17:36:02 +0100

    editing the cards components

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -9,18 +9,18 @@ export const Card: React.FC<CardsProps> = ({ title, text, price }) => {
   return (
     <div className=" mt-8 text-left bg-white border border-gray-200 rounded-xl shadow-xl sm:p-6  card-component ">
       <h5 className="mb-2 text-3xl font-bold text-gray-900 ">{title}</h5>
-      <div className="row flex d-flex">
-        <div className="col-md-4 ">
-          <p className="mb-5 pr-5 text-gray-800 sm:text-lg  text-left">
-            dangerouslySetInnerHTML={{ __html: text }}
+      <div className="flex d-flex">
+        <div>
+          <p className="mb-5 pr-5 text-gray-800 text-lg  text-left font-bold">
+            {text}
           </p>
         </div>
-        <div className="col-md-4">
+        <div>
           <p className="pl-11 font-bold text-gray-800 text-3xl card-component-price">
             {price}€
           </p>
         </div>
-        <div className="col-md-4 flex items-center mb-10 pl-8 card-component-checkbox">
+        <div className=" flex items-center mb-10 pl-8 card-component-checkbox">
           <input
             id="default-checkbox"
             type="checkbox"
